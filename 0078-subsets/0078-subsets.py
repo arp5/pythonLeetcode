4edead392d47c603b159640aeba0ead4c4fbd7f5
Arpita Awasthi <arpita.awasthi5@gmail.com>
2024-06-22 20:42:06 -0700

    Time: 33 ms (81.23%), Space: 16.8 MB (7.01%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,16 +1,13 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         ans = []
-        def ss(index,cur_comb):
-            if cur_comb not in ans:
-                ans.append(cur_comb.copy())
-            if index>=len(nums):
+        def bt(i,cur_comb):
+            if i>=len(nums):
+                ans.append(cur_comb[:])
                 return
-            for i in range(index,len(nums)):
-                if nums[i] not in cur_comb:
-                    cur_comb.add(nums[i])
-                    ss(index+1,cur_comb)
-                    cur_comb.remove(nums[i])
-        ss(0,set())
+            cur_comb.append(nums[i])
+            bt(i+1,cur_comb)
+            cur_comb.pop()
+            bt(i+1,cur_comb)
+        bt(0,[])
         return ans
-            
