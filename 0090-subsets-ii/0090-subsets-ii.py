65f8ba15e74228b6a0693d3baca64d80b7c21f2b
Arpita Awasthi <arpita.awasthi5@gmail.com>
2024-06-22 21:02:31 -0700

    Time: 40 ms (53.36%), Space: 16.7 MB (75.69%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.py
+++ 0090-subsets-ii/0090-subsets-ii.py
@@ -4,13 +4,13 @@ class Solution:
         nums.sort()
         def bt(i,cur_comb):
             if i>=len(nums):
-                if cur_comb not in ans:
-                    ans.append(cur_comb[:])
-                #ans.add(tuple(cur_comb[:]))
+                ans.append(cur_comb[:])
                 return
             cur_comb.append(nums[i])
             bt(i+1,cur_comb)
             cur_comb.pop()
-            bt(i+1, cur_comb)
+            while i+1<len(nums) and nums[i]==nums[i+1]:
+                i+=1
+            bt(i+1,cur_comb)
         bt(0,[])
         return ans
