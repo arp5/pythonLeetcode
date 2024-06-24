2eda78687893527fc8dd99a9083b86d8c1d6822d
Arpita Awasthi <arpita.awasthi5@gmail.com>
2024-06-24 00:50:52 -0700

    Time: 656 ms (15.68%), Space: 164.7 MB (13.69%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.py
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -2,9 +2,12 @@ class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
         self.count = 0
         combs = []
-        @lru_cache(maxsize=None)
+        dp = {}
+        #@lru_cache(maxsize=None)
         def rec(index,cur_sum):
             #print(index,cur_sum,cur_comb)
+            if (index,cur_sum) in dp:
+                return dp[(index,cur_sum)]
             if cur_sum == amount:
                 #self.count+=1
                 #combs.append(cur_comb[:])
@@ -17,7 +20,8 @@ class Solution:
             #cur_sum-=coins[index]
             #cur_comb.pop()
             #rec(index+1,cur_sum, cur_comb)
-            return rec(index,cur_sum+coins[index])+rec(index+1,cur_sum)
+            dp[(index,cur_sum)] = rec(index,cur_sum+coins[index])+rec(index+1,cur_sum)
+            return dp[(index,cur_sum)]
         n = len(coins)
         return rec(0,0)
         #print(combs)
