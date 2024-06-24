6631b193622d5bbe2eb52469cbb5607713cb3d59
Arpita Awasthi <arpita.awasthi5@gmail.com>
2024-06-24 00:08:15 -0700

    Time: 630 ms (56.76%), Space: 18.9 MB (53.46%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -1,21 +1,19 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
-        n,total= len(nums),sum(nums)
-        self.ans = False
-        @lru_cache(maxsize=None)
-        def rec(index,cur_sum):
-            #base
-            #print(index,cur_sum)
-            if cur_sum==total-cur_sum:
-                #self.ans = True
-                return True
-            if index>=n or cur_sum>total-cur_sum:
-                return False
-            #cur_sum+=nums[index]
-            #cur_comb.append(nums[index])
-            return rec(index+1,cur_sum+nums[index]) or rec(index+1,cur_sum)
-            #cur_comb.pop()
-            #cur_sum-=nums[index]
-            #rec(index+1,cur_sum)
-        return rec(0,0)
-        #return self.ans
+        dp = set()
+        dp.add(0)
+        total = sum(nums)
+        if total%2:
+            return False
+        target = total//2
+        for i in range(len(nums)-1,-1,-1):
+            nextdp = set()
+            for ts in dp:
+                #if ts+nums[i] in dp:
+                #    return True
+                nextdp.add(ts+nums[i])
+                nextdp.add(ts)
+            dp = nextdp
+        return True if target in dp else False
+            
+                
