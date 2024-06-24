48093b07b31e8a8f9d94047538b26851e7fc3700
Arpita Awasthi <arpita.awasthi5@gmail.com>
2024-06-24 00:46:32 -0700

    Time: 232 ms (20.97%), Space: 37.7 MB (42.6%) - LeetHub

--- 0494-target-sum/0494-target-sum.py
+++ 0494-target-sum/0494-target-sum.py
@@ -2,10 +2,13 @@ class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
         self.count = 0
         ans = []
-        @lru_cache(maxsize=None)
+        dp = {}
+        #@lru_cache(maxsize=None)
         def rec(index,cur_sum):
             #base
             #print(index,cur_sum, cur_comb)
+            if (index,cur_sum) in dp:
+                return dp[(index,cur_sum)]
             if cur_sum==target and index==len(nums):
                 #self.count+=1
                 #ans.append(cur_comb[:])
@@ -19,7 +22,8 @@ class Solution:
             #cur_sum-=(2*nums[index])
             #cur_comb.append(-nums[index])
             #rec(index+1,cur_sum)
-            return rec(index+1,cur_sum+nums[index]) + rec(index+1,cur_sum-nums[index])
+            dp[(index,cur_sum)] =  rec(index+1,cur_sum+nums[index]) + rec(index+1,cur_sum-nums[index])
+            return dp[(index,cur_sum)]
         return rec(0,0)
         #print(ans)
         #return self.count
