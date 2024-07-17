fc43b38c73f194f3d09bab92365ef12c825dbeba
Arpita Awasthi <arpita.awasthi5@gmail.com>
2024-07-16 17:24:43 -0700

    Time: 89 ms (61.18%), Space: 18.4 MB (40.81%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,22 +1,22 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        indegree = [0]*numCourses
         adjList = {}
+        indegree = [0]*numCourses
         for i in range(numCourses):
             adjList[i] = []
         for c1,c2 in prerequisites:
             adjList[c2].append(c1)
             indegree[c1]+=1
         queue = []
-        visited = 0
+        visited = set()
         for i in range(numCourses):
             if indegree[i]==0:
                 queue.append(i)
         while queue:
-            c = queue.pop(0)
-            visited+=1
-            for n in adjList[c]:
-                indegree[n]-=1
-                if indegree[n]==0:
-                    queue.append(n)
-        return visited==numCourses
+            course = queue.pop(0)
+            visited.add(course)
+            for c in adjList[course]:
+                indegree[c]-=1
+                if indegree[c]==0:
+                    queue.append(c)
+        return len(visited)==numCourses
