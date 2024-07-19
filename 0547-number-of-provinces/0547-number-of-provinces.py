4e07254999b41675a5aa76b7d5f1e14005c71a6b
Arpita Awasthi <arpita.awasthi5@gmail.com>
2024-07-18 17:39:22 -0700

    Time: 200 ms (14.94%), Space: 17.3 MB (71.68%) - LeetHub

---
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -0,0 +1,28 @@
+class Solution:
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        n = len(isConnected)
+        par = [i for i in range(n)]
+        rank = [1]*n
+        def find(n1):
+            res = n1
+            while res!=par[res]:
+                par[res] = par[par[res]]
+                res = par[res]
+            return res
+        def union(n1,n2):
+            p1,p2 = find(n1), find(n2)
+            if p1==p2:
+                return 0
+            if rank[p1]>rank[p2]:
+                par[p2] = par[p1]
+                rank[p1]+=1
+            else:
+                par[p1] = par[p2]
+                rank[p2]+=1
+            return 1
+        comp = n
+        for i in range(n):
+            for j in range(n):
+                if isConnected[i][j]==1:
+                    comp-=union(i,j)
+        return comp
