d91b6d3aa64640bdb3cd30c575ee9c285cdd89cc
Arpita Awasthi <arpita.awasthi5@gmail.com>
2024-07-28 16:57:47 -0700

    Time: 65 ms (98.39%), Space: 16.3 MB (98.33%) - LeetHub

--- 0777-toeplitz-matrix/0777-toeplitz-matrix.py
+++ 0777-toeplitz-matrix/0777-toeplitz-matrix.py
@@ -1,12 +1,13 @@
 class Solution:
     def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
-        matrixm = {}
-        n,m = len(matrix), len(matrix[0])
-        for i in range(n):
-            for j in range(m):
-                if j-i not in matrixm:
-                    matrixm[j-i] = matrix[i][j]
-                else:
-                    if matrix[i][j] != matrixm[j-i]:
-                        return False
-        return True
+        # matrixm = {}
+        # n,m = len(matrix), len(matrix[0])
+        # for i in range(n):
+        #     for j in range(m):
+        #         if j-i not in matrixm:
+        #             matrixm[j-i] = matrix[i][j]
+        #         else:
+        #             if matrix[i][j] != matrixm[j-i]:
+        #                 return False
+        # return True
+        return all(r==0 or c==0 or matrix[r-1][c-1]== val for r,row in enumerate(matrix) for c,val in enumerate(row))
