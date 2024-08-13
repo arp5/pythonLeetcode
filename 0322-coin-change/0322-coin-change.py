0ff06f2cf19e32e6a36176b7a1185bfe4b791557
Arpita Awasthi <arpita.awasthi5@gmail.com>
2024-08-13 01:42:43 -0700

    Time: 796 ms (51.36%), Space: 17.2 MB (35.86%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,11 +1,26 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
+        # self.coins = float('inf')
+        # def cc(cursum,curset,i):
+        #     if cursum==amount:
+        #         self.coins = min(self.coins,len(curset))
+        #         return
+        #     if i==len(coins) or cursum>amount:
+        #         return
+        #     curset.append(coins[i])
+        #     cc(cursum+coins[i],curset,i)
+        #     cc(cursum+coins[i],curset,i+1)
+        #     curset.pop()
+        #     cc(cursum,curset,i+1)
+        # cc(0,[],0)
+        # return self.coins if self.coins!=float('inf') else -1
         dp = [amount+1]*(amount+1)
-        dp[0] = 0
-        for a in range(amount+1):
-            for c in coins:
-                #print(a-c)
-                if a-c>=0:
-                    dp[a] = min(dp[a], 1+dp[a-c])
-        #print(dp)
-        return dp[amount] if dp[amount]!=amount+1 else -1
+        dp[-1]=0
+        for i in reversed(range(amount+1)):
+            if dp[i]!=amount+1:
+                for c in coins:
+                    if i-c>=0:
+                        dp[i-c]=min(1+dp[i],dp[i-c])
+        print(dp)
+        return dp[0] if dp[0]!=amount+1 else -1
+            
