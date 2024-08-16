7a51603d9dd24963c45319f6807fd6150d60067f
Arpita Awasthi <arpita.awasthi5@gmail.com>
2024-08-16 00:24:37 -0700

    Time: 81 ms (94.73%), Space: 18.2 MB (77.72%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -1,22 +1,25 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        prereq = {}
         ans = []
-        adjList = {}
         indegree = [0]*numCourses
         for i in range(numCourses):
-            adjList[i] = []
-        for c1,c2 in prerequisites:
-            adjList[c2].append(c1)
-            indegree[c1]+=1
+            prereq[i] = []
+        for course,pre in prerequisites:
+            prereq[pre].append(course)
+            indegree[course]+=1
         queue = []
         for i in range(numCourses):
-            if indegree[i] == 0:
+            if indegree[i]==0:
                 queue.append(i)
         while queue:
             course = queue.pop(0)
             ans.append(course)
-            for c in adjList[course]:
+            for c in prereq[course]:
                 indegree[c]-=1
                 if indegree[c]==0:
                     queue.append(c)
         return ans if len(ans)==numCourses else []
+#TC: O(N); SC: O(N)
+
+        
