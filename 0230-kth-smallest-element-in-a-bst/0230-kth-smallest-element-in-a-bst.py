a9f4070f6182f21a806c3313bde80779650bee5d
Arpita Awasthi <arpita.awasthi5@gmail.com>
2024-08-15 18:32:41 -0700

    Time: 55 ms (6.33%), Space: 19.5 MB (16.28%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
@@ -6,16 +6,32 @@
 #         self.right = right
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-        inarray = []
-        def inorder(root):
-            if not root:
-                return
-            inorder(root.left)
-            inarray.append(root.val)
-            #print((inarray), len(inarray), k)
-            if len(inarray) == k:
-                return
-            inorder(root.right)
-        inorder(root)
-        #print((inarray), len(inarray))
-        return inarray[k-1]
+        # inarray = []
+        # def inorder(root):
+        #     if not root:
+        #         return
+        #     inorder(root.left)
+        #     inarray.append(root.val)
+        #     if len(inarray) == k:
+        #         return
+        #     inorder(root.right)
+        # inorder(root)
+        # return inarray[k-1]
+        #TC: O(N); SC:O(N)
+        stack, ans = [],[]
+        stack.append(root)
+        node = root
+        while stack:
+            #print(stack)
+            while node and node.left:
+                stack.append(node.left)
+                node = node.left
+            node = stack.pop()
+            ans.append(node.val)
+            print(ans)
+            if len(ans)==k:
+                return ans[k-1]
+            if node.right:
+                stack.append(node.right)
+            node = node.right
+        return -1
