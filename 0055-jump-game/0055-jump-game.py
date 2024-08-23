08dd59436286531ea3dcda72575571cf3c69cfaf
Arpita Awasthi <arpita.awasthi5@gmail.com>
2024-08-23 15:55:32 -0700

    Time: 9476 ms (5%), Space: 18.1 MB (7.85%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.py
@@ -0,0 +1,11 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        n = len(nums)
+        dp = [False]*n
+        dp[-1] = True
+        for i in reversed(range(n-1)):
+            for j in range(i+1,min(i+nums[i]+1,n)):
+                if dp[j]:
+                    dp[i] = True
+        print(dp)
+        return dp[0]
