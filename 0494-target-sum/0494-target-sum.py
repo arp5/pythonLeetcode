705d37b59ceff9db9a868f3efa2e00187da4766b
Arpita Awasthi <arpita.awasthi5@gmail.com>
2024-08-24 14:50:16 -0700

    Time: 227 ms (24.7%), Space: 37.7 MB (39.37%) - LeetHub

--- 0494-target-sum/0494-target-sum.py
+++ 0494-target-sum/0494-target-sum.py
@@ -1,29 +1,11 @@
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
-        self.count = 0
-        ans = []
         dp = {}
-        #@lru_cache(maxsize=None)
-        def rec(index,cur_sum):
-            #base
-            #print(index,cur_sum, cur_comb)
-            if (index,cur_sum) in dp:
-                return dp[(index,cur_sum)]
-            if cur_sum==target and index==len(nums):
-                #self.count+=1
-                #ans.append(cur_comb[:])
-                return 1
-            if index>=len(nums):
-                return 0
-            #cur_sum+=nums[index]
-            #cur_comb.append(nums[index])
-            #rec(index+1,cur_sum)
-            #cur_comb.pop()
-            #cur_sum-=(2*nums[index])
-            #cur_comb.append(-nums[index])
-            #rec(index+1,cur_sum)
-            dp[(index,cur_sum)] =  rec(index+1,cur_sum+nums[index]) + rec(index+1,cur_sum-nums[index])
-            return dp[(index,cur_sum)]
-        return rec(0,0)
-        #print(ans)
-        #return self.count
+        def backtrack(index,cursum):
+            if index==len(nums):
+                return 1 if cursum==target else 0
+            if (index,cursum) in dp:
+                return dp[(index,cursum)]
+            dp[(index,cursum)] = backtrack(index+1,cursum+nums[index])+backtrack(index+1,cursum-nums[index])
+            return dp[(index,cursum)]
+        return backtrack(0,0)
